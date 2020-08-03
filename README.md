bcd07e8661c3da4e695f2f6d131bf97dd5a82f17
Jonas Dieker <jonasdieker05@gmail.com>
2020-08-03 23:21:40 +0200

    added pie chart and dropdown

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Covid Data Analysis for Germany
 
-Data from JHU's [Github](https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series)
+*Using data from JHU's [Github](https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series)*
 
 ![dashboard](/assets/dashboard.png)
--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import dash
 import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
-import json
+import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -18,21 +17,52 @@ data_deaths = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master
 data_recovered = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
 
 # graph styling
-layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', margin=dict(t=30))
+layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', margin=dict(t=30, b=70, l=50, r=50))
+
+# test pie chart (static)
+pie_chart = go.Figure(layout=layout)
+labels = ['Germany', 'US', 'United Kingdom', 'France', 'Spain']
+population = [83.0, 328.2, 66.7, 67.0, 46.9]
+values = []
+for i, country in enumerate(labels):
+	cum_cases = data_cases.loc[data_cases["Country/Region"] == country].to_numpy()
+	value = 0
+	if len(cum_cases) > 1:
+		for j in range(len(cum_cases)):
+				value += cum_cases[j][-1]
+		values.append(round(value/population[i]))
+	else:
+		values.append(round(cum_cases[0][-1]/population[i]))
+pie_chart.add_trace(go.Pie(labels=labels, values=values, hole=0.3))
+pie_chart.update_layout(title='Cases per million', uniformtext_minsize=10, uniformtext_mode='hide')
+pie_chart.update_traces(textposition='inside', textinfo='percent+label')
 
 # html of dashboard
 app.layout = html.Div(
 	children=[
 			html.H1(children='COVID DATA BY COUNTRY'),
 			html.P(children='Data from JHU, presented by Jonas Dieker'),
-			dcc.Dropdown(className='selectDrop', id='dropdown',
-						options=[{'label': 'Germany', 'value': 'Germany'},
-								{'label': 'Austria', 'value': 'Austria'},
-								{'label': 'USA', 'value': 'US'}],
-								value='Germany'),
+			html.Div(className='selectDrop row', children=
+				[dcc.Dropdown(className='six columns', id='dropdown',
+							options=[{'label': 'Germany', 'value': 'Germany'},
+							{'label': 'USA', 'value': 'US'},
+							{'label': 'United Kingdom', 'value': 'United Kingdom'},
+							{'label': 'France', 'value': 'France'},
+							{'label': 'Spain', 'value': 'Spain'},],
+							value='Germany'),
+				dcc.Dropdown(className='six columns', id='dropdown2',
+							options=[{'label': 'Cases', 'value': 'cases'},
+							{'label': 'Deaths', 'value': 'deaths'},
+							{'label': 'Recoveries', 'value': 'recovered'},
+							{'label': 'Active Cases', 'value': 'active'}],
+							value='cases')
+				]),
 			#dcc.Slider(className='selectSlider', min=0, max=len(daily_cases), marks={i: 'Days {}'.format(i) for i in range(0, len(daily_cases), 10)}, value=len(daily_cases)),
-			dcc.Graph(id='corona', config={'displayModeBar': False}, animate=True, style={}),
-			dcc.Graph(id='active_cases', config={'displayModeBar': False}),
+			dcc.Graph(id='first_fig', config={'displayModeBar': False}),
+			html.Div(className='row', children=
+				[dcc.Graph(className='four columns', id='third_fig', figure=pie_chart, config={'displayModeBar': False}),
+				dcc.Graph(className='eight columns', id='second_fig', config={'displayModeBar': False})
+				]),
 			html.Div(id='intermediate-value', style={'display': 'none'}),
 ])
 
@@ -42,12 +72,20 @@ app.layout = html.Div(
 def clean_data(dropdown_value):
 
 	country = str(dropdown_value)
-	germany_cases = data_cases.loc[data_cases["Country/Region"] == country]
-	germany_deaths = data_deaths.loc[data_deaths["Country/Region"] == country]
-	germany_recovered = data_recovered.loc[data_recovered["Country/Region"] == country]
-	cases = germany_cases.to_numpy()[0][4:]
-	deaths = germany_deaths.to_numpy()[0][4:]
-	recovered = germany_recovered.to_numpy()[0][4:]
+	country_cases = data_cases.loc[data_cases["Country/Region"] == country].to_numpy()
+	country_deaths = data_deaths.loc[data_deaths["Country/Region"] == country].to_numpy()
+	country_recovered = data_recovered.loc[data_recovered["Country/Region"] == country].to_numpy()
+
+	data_items = len(country_cases[0][4:])
+	cases = np.zeros(data_items)
+	deaths = np.zeros(data_items)
+	recovered = np.zeros(data_items)
+
+	for i in range(0, len(country_cases)):
+		for j in range(4, len(country_cases[0])):
+			cases[j-4] += abs(country_cases[i][j])
+			deaths[j-4] += abs(country_deaths[i][j])
+			recovered[j-4] += abs(country_recovered[i][j])
 
 	# reformatting to daily data
 	daily_cases = [0]
@@ -67,7 +105,7 @@ def clean_data(dropdown_value):
 	return df.to_json()
 
 
-@app.callback(Output('corona', 'figure'), [Input('intermediate-value', 'children')])
+@app.callback(Output('first_fig', 'figure'), [Input('intermediate-value', 'children')])
 def update_figure1(jsonified_df):
 
 	df = pd.read_json(jsonified_df)
@@ -75,38 +113,20 @@ def update_figure1(jsonified_df):
 	fig.add_trace(go.Bar(x=df["days"], y=df["cases"],name="Cases"))
 	fig.add_trace(go.Bar(x=df["days"], y=df["deaths"],name="Deaths"))
 	fig.add_trace(go.Bar(x=df["days"], y=df["recovered"],name="Recoveries"))
+	fig.update_layout(xaxis_title="Days from Start of Pandemic")
 
-	fig.update_layout(updatemenus=[dict(
-			visible=True,
-			active=0,
-			buttons=list([
-				dict(label="All",
-					 method="update",
-					 args=[{"visible": [True, True, True]}]),
-				dict(label="Cases",
-					 method="update",
-					 args=[{"visible": [True, False, False]}]),
-				dict(label="Deaths",
-					 method="update",
-					 args=[{"visible": [False, True, False]}]),
-				dict(label="Recoveries",
-					 method="update",
-					 args=[{"visible": [False, False, True]}]),
-			]),
-		)
-	])
 	return fig
 
 
-@app.callback(Output('active_cases', 'figure'), [Input('intermediate-value', 'children')])
-def update_figure2(jsonified_df):
+@app.callback(Output('second_fig', 'figure'), [Input('intermediate-value', 'children'), Input('dropdown2', 'value')])
+def update_figure2(jsonified_df, type_data):
 
 	df = pd.read_json(jsonified_df)
-	fig2 = go.Figure(layout=layout)
-	fig2.add_trace(go.Bar(x=df["days"], y=df["active"], name="Active Cases"))
-	fig2.update_layout(xaxis_title="Days from Start of Pandemic", yaxis_title="Active Case Numbers")
+	fig = go.Figure(layout=layout)
+	fig.add_trace(go.Bar(x=df["days"], y=df[type_data], name="Active Cases"))
+	fig.update_layout(xaxis_title="Days from Start of Pandemic")
 
-	return fig2
+	return fig
 
 
 
