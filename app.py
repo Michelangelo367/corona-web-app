7dc6bd689cad9279c5e15ae170bb4a902915281e
Jonas Dieker <jonasdieker05@gmail.com>
2020-08-03 02:25:49 +0200

    start of interactive dashboard

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__) #external_stylesheets=external_stylesheets)
 
 data_cases = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 data_deaths = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
@@ -29,29 +31,55 @@ daily_cases = [0]
 daily_deaths = [0]
 daily_recovered = [0]
 for i in range(1,len(cases)):
-    daily_cases.append(cases[i]-cases[i-1])
-    daily_deaths.append(deaths[i]-deaths[i-1])
-    daily_recovered.append(recovered[i]-recovered[i-1])
+	daily_cases.append(cases[i]-cases[i-1])
+	daily_deaths.append(deaths[i]-deaths[i-1])
+	daily_recovered.append(recovered[i]-recovered[i-1])
 daily_deaths = [abs(num) for num in daily_deaths]
 
 
 df = pd.DataFrame({"days": range(1, len(daily_cases)+1), "cases": daily_cases,
 					"deaths": daily_deaths, "recovered": daily_recovered})
 
-fig = px.bar(df, x="days", y="cases", barmode="group")
+fig = go.Figure()
+fig.add_trace(go.Bar(x=df["days"], y=df["cases"],name="Cases"))
+fig.add_trace(go.Bar(x=df["days"], y=df["deaths"],name="Deaths"))
+fig.add_trace(go.Bar(x=df["days"], y=df["recovered"],name="Recoveries"))
 
-app.layout = html.Div(children=[
-    html.H1(children='COVID DATA GERMANY'),
+fig.update_layout(updatemenus=[dict(
+			visible=True,
+			active=0,
+			buttons=list([
+				dict(label="All",
+					 method="update",
+					 args=[{"visible": [True, True, True]}]),
+				dict(label="Cases",
+					 method="update",
+					 args=[{"visible": [True, False, False]}]),
+				dict(label="Deaths",
+					 method="update",
+					 args=[{"visible": [False, True, False]}]),
+				dict(label="Recoveries",
+					 method="update",
+					 args=[{"visible": [False, False, True]}]),
+			]),
+		)
+	])
 
-    html.Div(children='''
-        Data from JHU and presented by Jonas Dieker
-    '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div(
+	children=[html.Div(className='row',
+		children=[html.Div(className='four columns div-user-controls',
+			children=[
+				html.H1('COVID DATA GERMANY'),
+				html.P('Data from JHU, presented by Jonas Dieker'),
+				]
+			),
+		html.Div(className='eight columns div-for-charts bg-grey',
+			children=[
+			dcc.Graph(id='corona', figure=fig, config={'displayModeBar': False}, animate=True)
+		])
+	])
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+	app.run_server(debug=True)
