4a20c858c383dcd4ddc92d282ee636c2e78e8391
Jonas Dieker <jonasdieker05@gmail.com>
2020-08-05 17:53:12 +0200

    added table

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 *Using data from JHU's [Github](https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series)*
 
+Note: this is not written to be perfect but simply to test the dash library.Generally this code is not formatted very nicely (especially the "HTML" part)
+
 ![dashboard](/assets/dashboard.png)
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import dash_table
 import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
@@ -19,10 +20,12 @@ data_recovered = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/mas
 # graph styling
 layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', margin=dict(t=30, b=70, l=50, r=50))
 
+
+#-static-stuff-just-as-a-test--------------------------------------------------------------------------------------------------------------
 # test pie chart (static)
 pie_chart = go.Figure(layout=layout)
-labels = ['Germany', 'US', 'United Kingdom', 'France', 'Spain']
-population = [83.0, 328.2, 66.7, 67.0, 46.9]
+labels = ['Germany', 'US', 'United Kingdom', 'France', 'Spain', 'Belgium', 'Argentina', 'Brazil', 'Russia', 'India', 'Mexico', 'Italy', 'Netherlands']
+population = [83.0, 328.2, 66.7, 67.0, 46.9, 11.5, 44.5, 209.5, 144.5, 1353.0, 126.2, 60.4, 17.3]
 values = []
 for i, country in enumerate(labels):
 	cum_cases = data_cases.loc[data_cases["Country/Region"] == country].to_numpy()
@@ -37,37 +40,103 @@ pie_chart.add_trace(go.Pie(labels=labels, values=values, hole=0.3))
 pie_chart.update_layout(title='Cases per million', uniformtext_minsize=10, uniformtext_mode='hide')
 pie_chart.update_traces(textposition='inside', textinfo='percent+label')
 
-# html of dashboard
-app.layout = html.Div(
+# test DataTable
+# creating data for table
+data_frames = [data_cases, data_deaths, data_recovered]
+data = [labels,[],[],[], []]
+
+for i, country in enumerate(labels):
+	for column in range(len(data_frames)):
+		cum_cases = data_frames[column].loc[data_frames[column]["Country/Region"] == country].to_numpy()
+		value = 0
+		if len(cum_cases) > 1:
+			for j in range(len(cum_cases)):
+					value += cum_cases[j][-1]
+			data[column+1].append(round(value))
+		else:
+			data[column+1].append(round(cum_cases[0][-1]))
+
+data[-1] = np.array(data[1]) - np.array(data[2]) - np.array(data[3])
+df_table = pd.DataFrame({'Country': data[0], 'Total Cases': data[1], 'Total Deaths': data[2], 'Total Recovered': data[3], 'Total Active': data[4]})
+df_table = df_table.sort_values(by=["Total Cases"], ascending=False)
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#-html-of-dashboard------------------------------------------------------------------------------------------------------------------------
+app.layout = html.Div(className='grey',
 	children=[
 			html.H1(children='COVID DATA BY COUNTRY'),
 			html.P(children='Data from JHU, presented by Jonas Dieker'),
-			html.Div(className='selectDrop row', children=
+						html.Div(className='grey selectDrop row', children=
 				[dcc.Dropdown(className='six columns', id='dropdown',
 							options=[{'label': 'Germany', 'value': 'Germany'},
 							{'label': 'USA', 'value': 'US'},
 							{'label': 'United Kingdom', 'value': 'United Kingdom'},
 							{'label': 'France', 'value': 'France'},
-							{'label': 'Spain', 'value': 'Spain'},],
-							value='Germany'),
+							{'label': 'Spain', 'value': 'Spain'},
+							{'label': 'Belgium', 'value': 'Belgium'},
+							{'label': 'Argentina', 'value': 'Argentina'},
+							{'label': 'Brazil', 'value': 'Brazil'},
+							{'label': 'Russia', 'value': 'Russia'},
+							{'label': 'India', 'value': 'India'},
+							{'label': 'Mexico', 'value': 'Mexico'},
+							{'label': 'Italy', 'value': 'Italy'},
+							{'label': 'Netherlands', 'value': 'Netherlands'},],
+							value='Germany',
+							clearable=False,
+							style={'backgroundColor': '#797979'}),
 				dcc.Dropdown(className='six columns', id='dropdown2',
 							options=[{'label': 'Cases', 'value': 'cases'},
 							{'label': 'Deaths', 'value': 'deaths'},
 							{'label': 'Recoveries', 'value': 'recovered'},
 							{'label': 'Active Cases', 'value': 'active'}],
-							value='cases')
-				]),
+							value='cases',
+							clearable=False,
+							style={'backgroundColor': '#797979'})
+				],),
 			#dcc.Slider(className='selectSlider', min=0, max=len(daily_cases), marks={i: 'Days {}'.format(i) for i in range(0, len(daily_cases), 10)}, value=len(daily_cases)),
-			dcc.Graph(id='first_fig', config={'displayModeBar': False}),
+			html.Div(className='row', children=
+				[html.Div(className='four columns', children=
+					[dash_table.DataTable(id='table', data=df_table.to_dict('records'), columns=[{'id': c, 'name':c} for c in df_table.columns], fixed_rows={'headers':True}, 
+					style_table={
+					'maxHeight': '50ex',
+					'overflowY': 'auto',
+					'overflowX': 'auto',
+					'width': '100%',
+					'minWidth': '100%',
+					},
+					style_data={
+					'whiteSpace': 'normal',
+					'height': 'auto',},
+					style_header={
+					'textAlign': 'left',
+					'backgroundColor': '#676767',
+					'fontWeight': 'bold',
+					'fontSize': 13},
+					style_cell={
+					'textAlign': 'left',
+					'backgroundColor': '#676767',
+					'color': '#B7B7B7'},
+					style_cell_conditional=[
+        			{'if': {'column_id': 'Country'},
+        			'width': '20%'},
+        			{'if': {'column_id': 'Total Recovered'},
+        			'width': '22%'},
+        			{'if': {'column_id': 'Total Active'},
+        			'width': '18%'},]
+					)]),
+				dcc.Graph(className='eight columns', id='first_fig', config={'displayModeBar': False})
+				]),
 			html.Div(className='row', children=
 				[dcc.Graph(className='four columns', id='third_fig', figure=pie_chart, config={'displayModeBar': False}),
 				dcc.Graph(className='eight columns', id='second_fig', config={'displayModeBar': False})
 				]),
 			html.Div(id='intermediate-value', style={'display': 'none'}),
 ])
+#------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
+#-helper-functions-------------------------------------------------------------------------------------------------------------------------
 @app.callback(Output('intermediate-value', 'children'), [Input('dropdown', 'value')])
 def clean_data(dropdown_value):
 
@@ -126,6 +195,7 @@ def update_figure2(jsonified_df, type_data):
 	fig.update_layout(xaxis_title="Days from Start of Pandemic")
 
 	return fig
+#------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
